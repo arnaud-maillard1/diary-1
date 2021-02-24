4b60f99778643ed9a69a88dfcef283eb357b6dd8
Yves Chevallier <yves.chevallier@heig-vd.ch>
2021-02-24 15:44:16 +0100

    Add arguments.c

---
+++ 1/argument.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+void version() {
+    printf(
+        "Version 0.1.1 Copyright HEIG-VD "
+        "<yves.chevallier@heig-vd.ch>"
+    );
+}
+/**
+ * char *argv[]
+ *         ^     Argv est
+ *           ^            un tableau
+ *      ^                            de pointeur
+ * ^                                 sur des caractères
+ *
+ * Un pointeur sur des caractères == Chaîne de caractère
+ * -> Tableau de chaînes de caractères
+ */
+int main(int argc, char *argv[]) {
+    // Traite les options sur les arguments
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "--version") == 0) {
+            version();
+            return 0;
+        }
+    }
+
+    // Affiche ARGument Count et les arguments
+    printf("Nombre d'arguments: %d\n", argc);
+    for (int i = 0; i < argc; i++) {
+        printf("%d. \"%s\"\n", i, argv[i]);
+    }
+}
